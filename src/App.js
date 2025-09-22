ca614b3f098bf45437b725e5e99497a972593d47
SweathaSureshpandy <lakshmirani497@gmail.com>
2025-09-22 11:55:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm Sweatha . This is my deployement React Application </h1>
+      <h1>Hi, I'm Sweatha. This is my deployement Application </h1>
     </div>
   );
 }
